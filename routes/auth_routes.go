fdf3cf5e5b4fb7eb207e18d52b33afd2db868d12
ariandto <92358210+ariandto@users.noreply.github.com>
2025-08-05 02:48:36 +0700

    oke2

--- routes/auth_routes.go
+++ routes/auth_routes.go
@@ -9,4 +9,5 @@ func RegisterAuthRoutes(r *gin.Engine) {
 	r.POST("/api/login", handlers.LoginWithFirebase)
 	r.POST("api/logout", handlers.Logout)
 	r.GET("/api/me", handlers.GetMe)
+	r.DELETE("/:id", handlers.DeleteProduct)
 }
--- routes/product_routes.go
+++ routes/product_routes.go
@@ -9,7 +9,7 @@ func RegisterProductRoutes(r *gin.Engine) {
 	api := r.Group("/api/products")
 	{
 		api.GET("", handlers.GetProducts)
-		api.POST("", handlers.CreateProduct) // gunakan CreateProduct, bukan upload
+		api.POST("/upload", handlers.UploadProduct)
 		api.PUT("/:id", handlers.UpdateProduct)
 		api.DELETE("/:id", handlers.DeleteProduct)
 	}
